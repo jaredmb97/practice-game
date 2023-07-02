04a225eecc23e414d28f2060e1083bd0eea07976
jaredmb97 <135680022+jaredmb97@users.noreply.github.com>
2023-07-02 00:34:15 -0600

    made game over trigger

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let character = document.getElementById("character");
+
 let block = document.getElementById("block");
+
 function jump() {
 	if (character.classList != "animate") {
 		character.classList.add("animate");
@@ -8,3 +10,15 @@ function jump() {
 		character.classList.remove("animate");
 	}, 500);
 }
+
+let checkDead = setInterval(function () {
+	let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+
+	let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+	if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+		block.style.animation = "none";
+		block.style.display = "none";
+		alert("game over");
+	}
+}, 10);
